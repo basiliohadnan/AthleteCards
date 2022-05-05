1e2f57aee5d24051796aac0ad0294e540fc7dba3
Hadnan Basilio | BT Créditos <hbasilio@bomtrato.com>
2022-05-05 10:52:39 -0300

    Profile Page | Route created, LASS changed, Routing method and Service created

--- AthleteCards/src/app/profile-page/profile-page.component.ts
+++ AthleteCards/src/app/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  private token: string = ''
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: any) => {
+      if (params && params.token) {
+        this.token = params.token
+      }
+    }) 
   }
 
 }
