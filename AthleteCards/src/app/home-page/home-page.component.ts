029c5bf2024084229950c15126b5cd89c56e05a0
Hadnan Basilio | BT Créditos <hbasilio@bomtrato.com>
2022-05-10 17:08:28 -0300

    Creating API first comm and token validator method

--- AthleteCards/src/app/home-page/home-page.component.ts
+++ AthleteCards/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProfilePageService } from '../profile-page/profile-page.service';
 
 @Component({
   selector: 'app-home-page',
@@ -9,11 +10,31 @@ export class HomePageComponent {
 
   token: string = ''
 
-  constructor() {
+  constructor(private profilePageService: ProfilePageService) {
   }
 
   receiveValue(value: string) {
     this.token = value
-    console.log(value)
+  }
+
+  validateToken(token: string) {
+    if (token.length < 40) {
+      return false
+    }
+    else {
+      return true
+    }
+  }
+
+  viewAthleteCard() {
+    if (!this.validateToken(this.token)) {
+      alert("Token must have at least 42 characters... Try again.")
+    }
+    else {
+      this.profilePageService.logActivityStats(this.token).subscribe(x => {
+        console.log(x);
+      })
+      this.profilePageService.loadProfilePage(this.token)
+    }
   }
 }
