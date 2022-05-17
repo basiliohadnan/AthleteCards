db9247ee714d653a681382cab201090f76f2d37e
Hadnan Basilio | BT Créditos <hbasilio@bomtrato.com>
2022-05-17 10:29:04 -0300

    Creating initial profile page' structure, implementing method that gets some api-brought info and read them there

--- AthleteCards/src/app/profile-page/profile-page.component.ts
+++ AthleteCards/src/app/profile-page/profile-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AllRunTotals } from '../shared/interfaces/all-run-totals';
+import { AthleteStats } from '../shared/interfaces/athlete-stats';
+import { ProfilePageService } from './profile-page.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -9,13 +12,24 @@ import { ActivatedRoute } from '@angular/router';
 export class ProfilePageComponent implements OnInit {
 
   private token: string = ''
+  athleteStats!: AthleteStats
+  athleteStatsAllRunTotals!: AllRunTotals
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private profilePageService: ProfilePageService) { }
+
+  logActivityStats() {
+    this.profilePageService.logActivityStats(this.token).subscribe((response: AthleteStats) => {
+      this.athleteStats = response
+      this.athleteStatsAllRunTotals = this.athleteStats.all_run_totals
+      console.log(this.athleteStatsAllRunTotals);
+    })
+  }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params: any) => {
       if (params && params.token) {
         this.token = params.token
+        this.logActivityStats()
       }
     })
   }
